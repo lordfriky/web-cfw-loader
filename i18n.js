38cc36a1f6056c051d3c269ba3d77f31b46bb119
Lord Friky <lord.friky.43@gmail.com>
2019-02-10 15:24:32 -0600

    Update language stuff

--- i18n.js
+++ i18n.js
@@ -16,10 +16,10 @@ function setPageLanguage(lang) {
 
   else if (lang === "es-MX")
     langObj = esMX;
-  
+
   else if (lang === "fr-FR")
     langObj = frFR;
-  
+
   else if (lang === "pl-PL")
     langObj = plPL;
 
@@ -55,7 +55,8 @@ function setPageLanguage(lang) {
   document.getElementById("optionCTCaerHekate").innerHTML  = langObj.optionCTCaerHekate;
   document.getElementById("optionFusee").innerHTML     = langObj.optionFusee;
   document.getElementById("optionSXOS").innerHTML      = langObj.optionSXOS;
-  document.getElementById("optionReiNX").innerHTML      = langObj.optionReiNX;
+  document.getElementById("optionReiNX").innerHTML     = langObj.optionReiNX;
+  document.getElementById("optionBriccmii").innerHTML  = langObj.optionBriccmii;
   document.getElementById("optionUpload").innerHTML    = langObj.optionUpload;
   document.getElementById("h4GetByteArray").innerHTML  = langObj.h4GetByteArray;
   document.getElementById("h3Log").innerHTML           = langObj.h3Log;
