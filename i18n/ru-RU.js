21aaa4e09634467e0463907e05c1b95928dc9fd6
matticcc <54719687+matticcc@users.noreply.github.com>
2022-03-10 18:06:53 +0100

    Update ru-RU.js

--- i18n/ru-RU.js
+++ i18n/ru-RU.js
@@ -18,7 +18,7 @@ let ruRU = {
   "optionAtmosphere" :"Atmosphère 0.19.2",
   "optionSXOS"      :"SX OS",
   "optionReiNX"     :"ReiNX 2.0",
-  "optionCTCaerHekate" :"CTCaer's Hekate Mod v5.5.5",
+  "optionCTCaerHekate" :"CTCaer's Hekate Mod v5.7.0",
   "optionFusee"     :"Тестовый пейлоад от (Re)Switched",
   "optionUpload"    :"Загрузить свой пейлоад",
   "h3Log"           :"Log:",
