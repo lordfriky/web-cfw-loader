c5d7798ae5cef9a9c55c7ebbf0faf9a108836f7a
matticcc <54719687+matticcc@users.noreply.github.com>
2022-03-10 18:06:05 +0100

    Update it-IT.js

--- i18n/it-IT.js
+++ i18n/it-IT.js
@@ -18,7 +18,7 @@ let itIT = {
   "optionAtmosphere" :"Atmosphère 0.19.2",
   "optionSXOS"      :"SX OS",
   "optionReiNX"     :"ReiNX 2.0",
-  "optionCTCaerHekate" :"CTCaer's Hekate Mod v5.5.5",
+  "optionCTCaerHekate" :"CTCaer's Hekate Mod v5.7.0",
   "optionFusee"     :"(Re)Switched payload di test (fusee)",
   "optionUpload"    :"Carica payload",
   "h3Log"           :"Log:",
